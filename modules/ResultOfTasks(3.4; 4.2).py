6a9e449577104ec4d7556c4b3e466c59d8e23b4c
LAPTOP-HFQ73LSO\HP <max99919991@gmail.com>
2017-05-26 00:12:02 +0300

    new

--- modules/ResultOfTasks(3.4; 4.2).py
+++ modules/ResultOfTasks(3.4; 4.2).py
@@ -7,26 +7,17 @@ from time import gmtime, strftime
 import time
 from datetime import datetime
 
-def check_time():
-    """
-    This function return exact time.
-    """
-    date = strftime("%d.%m.%Y %H:%M:%S", gmtime())
-    return date
+date = strftime("%d.%m.%Y %H:%M:%S", gmtime()) #exact time.
+
 
 def code_time():
     """
-    This function return coded time by timestamp coding
+    This function return number of coded time by timestamp coding.
     """
-    d = datetime.strptime(check_time(), "%d.%m.%Y %H:%M:%S")
+    d = datetime.strptime(date, "%d.%m.%Y %H:%M:%S")
     ts = time.mktime(d.timetuple())
     return int(ts)
 
-print check_time()
-print code_time()
-
-
-# Authenticate via OAuth
 client = pytumblr.TumblrRestClient(
     consumer_key,
     consumer_secret,
@@ -37,14 +28,21 @@ client = pytumblr.TumblrRestClient(
 main_list = client.tagged('sience', before=code_time())
 
 prime_dict = {}
-for i in main_list:
-    if i['note_count'] not in prime_dict:
-        prime_dict[i['note_count']] = set()
-        prime_dict[i['note_count']].add(i["post_url"])
-    else:
-        prime_dict[i['note_count']].add(i["post_url"])
+
+def main_dict():
+    """
+    This function return dictionary where keys are count of notes
+    and values are links of posts.
+    """
+    for i in main_list:
+        if i['note_count'] not in prime_dict:
+            prime_dict[i['note_count']] = set()
+            prime_dict[i['note_count']].add(i["post_url"])
+        else:
+            prime_dict[i['note_count']].add(i["post_url"])
+    return prime_dict
 
 
-for i in reversed(prime_dict.keys()):
-    for j in prime_dict[i]:
+for i in reversed(main_dict().keys()):
+    for j in main_dict()[i]:
         print i, "-", j
